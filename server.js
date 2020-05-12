1045841e3fb0e43573b937e2739c8634d7781f54
Mich1300 <43409812+Mich1300@users.noreply.github.com>
2020-05-11 22:32:23 -0500

    server

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 // impotamos los modulos
-
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const express = require('express');
 const exhandle = require('express-handlebars');
 
-// importamos los productos
-
-const products = require('./products');
+// importamos las rutas
+const configureRoutes = require('./routes');
 
 // creamos el servidor
-
 const app = express();
 
 // configuramos la carpeta publica
-
 app.use(express.static('public'));
 
 //configuramos handlebars
@@ -20,14 +18,27 @@ app.use(express.static('public'));
 app.engine('handlebars', exhandle());
 app.set('view engine', 'handlebars');
 
-//conectar cositas con chrome
-app.get('/', function (req, res) {
+// Connection URL
+const url = 'mongodb://localhost:27017';
 
-     console.log('Holi en la consola');
-     res.send('hola en chrome');
-    
-    });
-app.listen(3000, function(){
+// Database Name
+const dbName = 'store';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function (err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    configureRoutes(app, db);
+});
+
+
+app.listen(3000, function () {
     console.log('servidor iniciado en el puerto 3000');
 });
 
